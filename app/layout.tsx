28a02d44ef56f827de763f5170a0797c8c800e92
unknown <imubashirahmad@gmail.com>
2024-02-22 15:04:46 +0500

    2/22 3pm commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import Header from './header'
-import Themes from './themes'
-import {ThemeProvider, createGlobalStyle} from 'styled-components'
-import {useState, createContext} from 'react'
+import Header from './header';
+import Themes from './themes';
+import styled, {ThemeProvider, createGlobalStyle} from 'styled-components';
+import {useState, createContext} from 'react';
+
+export const useClient = () => null;
 
 export default function RootLayout({
   children,
@@ -9,11 +11,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>){
   const [theme, setTheme] = useState('light');
+  
+  const GlobalStyle = createGlobalStyle`
+    body {
+      margin: 0;
+      padding: 0;
+    }
+  `;
+
+  const LayoutWrapper = styled.div`
+  min--height: 100vh;
+  background-color: ${(props) => props.theme.bgColor};
+  background-image: ${(props) => props.theme.bgImage};
+  color: ${(props) => props.color};
+  `;
+
+  const changeTheme = ()=>
+  {
+    setTheme(theme == "light" ? "dark": "light");
+  }
   return (
     <>
     <ThemeProvider theme={Themes[theme]}>
-    <Header></Header>
+    <LayoutWrapper onClick={changeTheme}>
+    <GlobalStyle />
+    <Header />
     {children}
+    </LayoutWrapper>
     </ThemeProvider></>
   );
 }
